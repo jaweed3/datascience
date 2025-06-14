c0fcbfa1b1d387207f1559eee259e632005bdca1
jaweed3 <wedjaw22@gmail.com>
2025-06-14 13:33:05 +0700

    add: main stage 1

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom Logger data science.")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/datascience/entity/config_entity.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from pathlib import Path
+from src.datascience.constant import *
+from src.datascience.utils.common import read_yaml, create_directories
+
+# Update the params.yaml
+@dataclass
+class DataIngestionConfig:
+    root_dir: Path
+    source_dir: str
+    local_data_file: Path
+    unzip_dir: Path
